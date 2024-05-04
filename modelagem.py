09c2ae05127324fe34dad8580999bf98b5d96769
atico0 <luisgabrielmarques12@gmail.com>
2024-05-04 18:45:11 -0300

    mudando o pcfacil de lugar

--- modelagem.py
+++ modelagem.py
@@ -19,3 +19,46 @@ def treinar_todos(x, y, modelos, funcs_perda):
   metricas = pd.DataFrame(perda)
   metricas.index = list(funcs_perda.keys())
   return(metricas.transpose())
+
+
+
+def pcfacil(df, n_comp, variaveis=[], test=[]):
+  """
+  reliza uma PCA e retorna um dataframe com as componentes principais e
+  retorna um df com os componentes principais e as variáveis não incluidas
+
+  df: dataframe
+  n_comp: inteiro, quantidade de componentes
+  variáveis: lista de variáveis para serem incluidas no PCA 
+  (caso nada seja imputado, usa a função pega_tipos)
+  test: dataframe, dataframe de teste para caso o argumento df seja o dataframe de treino
+
+  """
+
+
+  if len(variaveis)==0:
+    cat, numericas = pega_tipos(df)
+    lista_cat = list(cat.keys())
+    lista_numericas = list(numericas.keys())
+    
+  else:
+    lista_numericas = variaveis
+    lista_cat = []
+    for j in range(df.shape[1]):
+      if j not in lista_numericas:
+        lista_cat.append(j)
+
+  pca = PCA(n_components=n_comp)
+  df_pca = pca.fit_transform(df.iloc[:, lista_numericas])
+  lista_nomes = []
+  for i in range(1, n_comp+1):
+    lista_nomes.append(f"componente_{i}")
+  df_pca = pd.DataFrame(df_pca, columns=lista_nomes)
+  df_pca = pd.concat([df.iloc[:, lista_cat], df_pca], axis=1)
+
+  if len(test) != 0:
+    test_pca = pca.transform(test.iloc[:, lista_numericas])
+    test_pca = pd.DataFrame(test_pca, columns=lista_nomes)
+    df_pca = pd.concat([test.iloc[:, lista_cat], test_pca], axis=1)
+    return([df_pca, test, pca])
+  return(df_pca, pca)
