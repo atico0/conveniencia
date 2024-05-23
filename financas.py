72a79be3b351dec2fbf6419d48efb552ad0eb7e5
atico0 <luisgabrielmarques12@gmail.com>
2024-05-22 21:46:49 -0300

    adicionando mais funções de finanças

--- financas.py
+++ financas.py
@@ -12,12 +12,12 @@ import yfinance as yf
 
 
 def retorno_simples(serie, inicio, final, quant=1, dividendo=0, taxa_compra=0, taxa_venda=0):
-  rs = ((serie[inicio] + dividendo - serie[final]) * quant - taxa_compra - taxa_venda)/serie[inicio]
+  rs = ((serie[final] + dividendo - serie[inicio]) * quant - taxa_compra - taxa_venda)/serie[inicio]
   return(rs*100)
 
 
 def lucro(serie, inicio, final, quant=1, dividendo=0, taxa_compra=0, taxa_venda=0):
-  l = (serie[inicio] + dividendo - serie[final]) * quant - taxa_compra - taxa_venda
+  l = (serie[final] + dividendo - serie[inicio]) * quant - taxa_compra - taxa_venda
   return l
 
 def baixa_acoes(acoes, variavel, inicio, fim=None):
@@ -25,5 +25,21 @@ def baixa_acoes(acoes, variavel, inicio, fim=None):
   for acao in acoes:
     acoes_df[acao] = yf.download(acao, start=inicio, end=fim).loc[:, variavel]
   return acoes_df
+
+
+def normalizador(df, variaveis):
+  acoes_df_normalizado = df.copy()
+  for i in acoes_df_normalizado.columns[1:]:
+    acoes_df_normalizado[i] = acoes_df_normalizado[i] / acoes_df_normalizado[i][0]
+  return acoes_df_normalizado
+
+def historico(df, variaveis, x=None, normalizar=False, figsize = (15,7),
+  title = 'Histórico do preço das ações'):
+  if normalizar:
+    acoes_df_normalizado = normalizador(df=df, variaveis=variaveis)
+    acoes_df_normalizado.loc[:, variaveis].plot(x = x, figsize = figsize,
+      title = 'Histórico do preço das ações')
+  else:
+    df.loc[:, variaveis].plot(x = x, figsize = figsize, title = 'Histórico do preço das ações')
 
 
