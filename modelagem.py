9b05891731600e73a8a8949a114f6b39a1be3621
atico0 <luisgabrielmarques12@gmail.com>
2024-05-25 10:33:13 -0300

    importando pca no modelagem

--- modelagem.py
+++ modelagem.py
@@ -1,6 +1,8 @@
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
+from sklearn.decomposition import PCA
+
 
 #modelos
 def treinar_todos(x, y, modelos, funcs_perda):
