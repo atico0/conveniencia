c973d9d461f1a6b510f9e2a59b09d0f5febe50a8
atico0 <luisgabrielmarques12@gmail.com>
2024-06-04 13:34:20 -0300

    adicionando os métodos de desfazer os encoders

--- engenharia_dados.py
+++ engenharia_dados.py
@@ -14,12 +14,29 @@ def labelencoder(df, lista):
   df: dataframe
   lista: lista númerica de variáves
   """
-  for i in lista:
-    df.iloc[:,i] = LabelEncoder().fit_transform(df.iloc[:,i])
-  return(df)
+
+  cols = df.columns[lista]
+  transformadores = {}
+  for i in cols:
+    transformador = LabelEncoder() 
+    df.loc[:,i] = transformador.fit_transform(df.loc[:,i])
+    transformadores[i] = transformador
+
+  return (df, transformadores)
+
+def desfaz_label(df, transformadores):
+  
+
+  for i in transformadores.keys():
+    df.loc[:, i] = transformadores[i].inverse_transform(df.loc[:, i])
+
+  return df
+
+
 
 #dados nominais
-def onehotencoder(df, lista, drop_first=True):
+
+def onehotencoder(df, lista, prefix_sep="_", drop_first=True):
   """
   realiza o one hot encoder num conjunto de variáveis num dataframe
 
@@ -27,11 +44,52 @@ def onehotencoder(df, lista, drop_first=True):
   lista: lista númerica de variáves
   """
   cols = df.columns[lista]
+  valores_dropados = {}
   for i in cols:
-    novo = pd.get_dummies(df.loc[:, i], prefix=i, drop_first=drop_first).astype(int)
+    novo = pd.get_dummies(df.loc[:, i], prefix=i,
+      prefix_sep=prefix_sep).astype(int)
+
+    for coluna in novo.columns:
+      elemento_coluna = coluna.replace(i+prefix_sep,"")
+
+    if drop_first:
+      valores_dropados[i] = novo.columns[0].replace(i+prefix_sep,"")
+      novo = novo.iloc[:, 1:]
     df = pd.concat([df, novo], axis=1)
     df.drop([i], axis=1, inplace=True)
+  return (df, valores_dropados)
+
+
+
+def desfaz_1onehote(df, var_org, valor_dropado="", prefix_sep="_"):
+  lista_variaveis = []
+    lista_valores = []
+
+    for j in df.columns:
+      if var_org in j:
+        lista_variaveis.append(j)
+    
+    for n in range(df.shape[0]):
+      local = np.where(np.array(df.loc[n, lista_variaveis]) == 1)[0]
+      if local:
+        lista_valores.append(lista_variaveis[local[0]].replace(i+prefix_sep,""))
+      else:
+        lista_valores.append(valor_dropado)
+        
+
+  df[var_org] = lista_valores
+  df.drop(lista_variaveis, axis=1, inplace=True)
   return df
+
+
+
+
+def desfaz_onehote(df, valores_dropados, prefix_sep="_", drop_first=True):
+
+  for i in valores_dropados.keys():
+    df = desfaz_1onehote(df, i, valores_dropados[i], prefix_sep)
+  return df
+
 
 
 
