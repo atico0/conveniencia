38f577a87e67a3e4570b0d9794b184ffe38f204c
atico0 <luisgabrielmarques12@gmail.com>
2024-06-08 19:20:55 -0300

    adicionando comentários nos gráficos dnv

--- graficos.py
+++ graficos.py
@@ -6,6 +6,21 @@ import pandas as pd
 import numpy as np
 
 def histogramas(df, colunas, forma, figsize= (20, 10)):
+  """
+  Plota vários histogramas de uma vez
+
+  df: Dataframe contendo as variáveis a serem plotadas
+
+  colunas: lista númerica representando as coordenadas das variáveis
+
+  forma: tupla numérica representando a quantidade de linhas e colunas dos histogramas
+
+  figsize: tupla numérica representando o tamanho de cada histograma
+
+  returns:
+  Nda
+  """
+
   num_cols = df.columns[colunas]
   fig, ax = plt.subplots(forma[0], forma[1], figsize=figsize)
 
@@ -17,6 +32,21 @@ def histogramas(df, colunas, forma, figsize= (20, 10)):
 
 
 def boxplots(df, colunas, forma, figsize= (20, 10)):
+  """
+  Plota vários boxplots de uma vez
+
+  df: Dataframe contendo as variáveis a serem plotadas
+
+  colunas: lista númerica representando as coordenadas das variáveis
+
+  forma: tupla numérica representando a quantidade de linhas e colunas dos plots
+
+  figsize: tupla numérica representando o tamanho de cada plot
+
+  returns:
+  Nda
+  
+  """
   cols = df.columns[colunas]
   fig, ax = plt.subplots(forma[0], forma[1], figsize=figsize)
 
@@ -28,17 +58,51 @@ def boxplots(df, colunas, forma, figsize= (20, 10)):
 
 import ptitprince as pt
 
-def rainclouds(df, colunas, forma, figsize= (20, 10), x=None):
+def rainclouds(df, colunas, forma, figsize= (20, 10), x=None, orient="h"):
+  
+  """
+  Plota vários rainclouds de uma vez]
+
+  df: Dataframe contendo as variáveis a serem plotadas
+
+  colunas: lista númerica representando as coordenadas das variáveis
+
+  forma: tupla numérica representando a quantidade de linhas e colunas dos plots
+
+  figsize: tupla numérica representando o tamanho de cada plot
+
+  x: string representando a variável categorica a ser usada para dividir os rainclouds (basicamente um hue)
+
+  returns:
+  Nda
+  """
   cols = df.columns[colunas]
   fig, ax = plt.subplots(forma[0], forma[1], figsize=figsize)
 
   for col, ax in zip(cols, ax.flatten()):
-    pt.RainCloud(data=df, y=col, x=x, orient="h", ax=ax)
+    pt.RainCloud(data=df, y=col, x=x, orient=orient, ax=ax)
   plt.tight_layout()
   plt.show()
 
 
 def barras_x(df, colunas, y, forma, figsize= (20, 10), hue=None):
+  """
+  Plota várias barras de uma vez alterando o eixo X
+  
+  df: Dataframe contendo as variáveis a serem plotadas
+
+  colunas: lista númerica representando as coordenadas das variáveis (eixos dos x)
+
+  y: string representando a variável do eixo y das barras
+
+  forma: tupla numérica representando a quantidade de linhas e colunas dos plots
+
+  figsize: tupla numérica representando o tamanho de cada plot
+
+  returns:
+  Nda
+  """
+
   cols = df.columns[colunas]
   fig, ax = plt.subplots(forma[0], forma[1], figsize=figsize)
 
@@ -49,6 +113,22 @@ def barras_x(df, colunas, y, forma, figsize= (20, 10), hue=None):
 
 
 def barras_y(df, colunas, x, forma, figsize= (20, 10), hue=None):
+  """
+  Plota várias barras de uma vez alterando o eixo Y
+  
+  df: Dataframe contendo as variáveis a serem plotadas
+
+  colunas: lista númerica representando as coordenadas das variáveis (eixos dos y)
+
+  x: string representando a variável do eixo x das barras
+
+  forma: tupla numérica representando a quantidade de linhas e colunas dos plots
+
+  figsize: tupla numérica representando o tamanho de cada plot
+
+  returns:
+  Nda
+  """
   cols = df.columns[colunas]
   fig, ax = plt.subplots(forma[0], forma[1], figsize=figsize)
 
