a5b4bb284fc1cbed644759b1f5bc8a41c4a85f5b
atico0 <luisgabrielmarques12@gmail.com>
2024-06-14 19:40:51 -0300

    atualizando o init pq faltou aaaaaaaaaa

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 __all__ = ["eng_dados", "graficos", "modelagem", "financas"]
 
 
-from .eng_dados import labelencoder, desfaz_label, onehotencoder, desfaz_1onehote, desfaz_onehote, padronizar, pega_tipos
+#from .eng_dados import labelencoder, desfaz_label, onehotencoder, desfaz_1onehote, desfaz_onehote, padronizar, pega_tipos
 
 from .graficos import histogramas, boxplots, rainclouds, barras_x, barras_y, dispersoes 
 
