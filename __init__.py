17d96f9ad027932508bfc1994f3a7c2ad0130155
atico0 <luisgabrielmarques12@gmail.com>
2024-06-14 19:29:54 -0300

    atualizando o init dnv

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-__all__ = ["eng_dados", "graficos", "modelagem"]
+__all__ = ["eng_dados", "graficos", "modelagem", "financas"]
 
 
 from .engenharia_dados import labelencoder, desfaz_label, onehotencoder
@@ -7,3 +7,5 @@ from .engenharia_dados import desfaz_1onehote, desfaz_onehote, padronizar, pega_
 from .graficos import histogramas, boxplots, rainclouds, barras_x, barras_y, dispersoes 
 
 from .modelagem import treinar_todos, pcfacil
+
+from .financas import retorno_simples, lucro, baixa_acoes, normalizador, historico
